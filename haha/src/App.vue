79a606a94a9800ae41a84d4ba3630382887633ea
slava <asperatuz@yandex.ru>
2022-04-26 15:12:51 +0900

    dsad

---
+++ haha/src/App.vue
@@ -0,0 +1,144 @@
+<template>
+  <div class="row">
+    <div class="col-4">
+      <h3>First draggable with footer</h3>
+
+      <draggable
+        id="first"
+        data-source="juju"
+        :list="list"
+        class="list-group"
+        group="a"
+        item-key="name"
+        :animation="300"
+      >
+        <template #item="{ element }">
+          <div class="list-group-item">
+            {{ element.name }}
+          </div>
+        </template>
+
+        <template #footer>
+          <div class="btn-group list-group-item" role="group">
+            <button class="btn btn-secondary" @click="add">Add</button>
+            <button class="btn btn-secondary" @click="replace">Replace</button>
+          </div>
+        </template>
+      </draggable>
+    </div>
+
+    <div class="col-4">
+      <h3>Second draggable with header</h3>
+
+      <draggable :list="list2" class="list-group" group="a" item-key="name" :animation="300">
+        <template #item="{ element }">
+          <div class="list-group-item item">
+            {{ element.name }}
+          </div>
+        </template>
+
+        <template #header>
+          <div
+            class="btn-group list-group-item"
+            role="group"
+            aria-label="Basic example"
+          >
+          </div>
+        </template>
+      </draggable>
+    </div>
+
+    <div class="col-4">
+      <h3>Second draggable with header</h3>
+
+      <draggable :list="list3" class="list-group" group="a" item-key="name" :animation="300">
+        <template #item="{ element }">
+          <div class="list-group-item item">
+            {{ element.name }}
+          </div>
+        </template>
+
+        <template #header>
+          <div
+            class="btn-group list-group-item"
+            role="group"
+            aria-label="Basic example"
+          >
+          </div>
+        </template>
+      </draggable>
+    </div>
+
+    <rawDisplayer class="col-2" :value="list" title="List" />
+
+    <rawDisplayer class="col-2" :value="list2" title="List2" />
+  </div>
+</template>
+
+<script>
+import draggable from "vuedraggable";
+let id = 1;
+export default {
+  name: "two-list-headerslots",
+  display: "Two list header slot",
+  order: 14,
+  components: {
+    draggable
+  },
+  data() {
+    return {
+      list: [
+        { name: "John 1", id: 0 },
+        { name: "Joao 2", id: 1 },
+        { name: "Jean 3", id: 2 }
+      ],
+      list2: [
+        { name: "Jonny 4", id: 3 },
+        { name: "Guisepe 5", id: 4 }
+      ],
+      list3: [
+        { name: "Jonny 5", id: 5 },
+        { name: "Guisepe 6", id: 6 }
+      ]
+    };
+  },
+  methods: {
+    add: function() {
+      this.list.push({ name: "Juan " + id, id: id++ });
+    },
+    replace: function() {
+      this.list = [{ name: "Edgard", id: id++ }];
+    },
+    add2: function() {
+      this.list2.push({ name: "Juan " + id, id: id++ });
+    },
+    replace2: function() {
+      this.list2 = [{ name: "Edgard", id: id++ }];
+    },
+    add3: function() {
+      this.list2.push({ name: "XDXDXDXD " + id, id: id++ });
+    },
+    replace3: function() {
+      this.list2 = [{ name: "dsdsadsadsa", id: id++ }];
+    }
+  }
+};
+</script>
+<style scoped>
+.buttons {
+  margin-top: 35px;
+}
+.ghost {
+  opacity: 0.5;
+  background: #c8ebfb;
+}
+.not-draggable {
+  cursor: no-drop;
+}
+.list-group-item {
+  padding: 20px;
+  background: gray;
+  border: 2px solid rgb(73, 67, 67);
+  margin: 10px 0;
+}
+</style>
